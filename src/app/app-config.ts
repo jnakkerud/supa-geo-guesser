510fa59e0be109a5208ec67d074dd4f8667b8228
Jon Nakkerud <jon@nubotech.com>
2023-05-19 11:16:11 -0700

    remove app-config

--- src/app/app-config.ts
+++
@@ -1,9 +0,0 @@
-export const supabaseConfig = {
-    /*supabaseUrl: 'https://pwkvntgpibdcldgfsvod.supabase.co',
-    supabaseFunctionsUrl: 'https://pwkvntgpibdcldgfsvod.functions.supabase.co',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3a3ZudGdwaWJkY2xkZ2Zzdm9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY3MzQ2MDUsImV4cCI6MTk4MjMxMDYwNX0.whflqjieaTvHIKnbMyiWTUTDm9iBDWne9OZyzsvn8Yw'*/
-};
-
-export const flickrConfig = {
-    apiKey: '25e4ac50b599c34a19a44c08339a3292'
-}
