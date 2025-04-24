0355ab401ca3008e9fee79efa58f5e4e5aaf00c2
Jon Nakkerud <jon@nubotech.com>
2025-04-24 10:55:18 -0700

    lazy load admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AdminComponent } from './admin/admin.component';
-import { EditImageComponent } from './admin/edit-image/edit-image.component';
 import { ThemePlayComponent } from './home/theme-play/theme-play.component';
 import { HomeComponent } from './home/home.component';
 import { ThemeResultComponent } from './home/theme-result/theme-result.component';
@@ -9,9 +7,12 @@ import { ThemeListComponent } from './home/theme-list/theme-list.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent},
-    { path: 'admin', component: AdminComponent},
+    { path: 'admin', 
+        loadComponent: () => import('./admin/admin.component').then(m => m.AdminComponent)},
     { path: 'themes', component: ThemeListComponent},
-    { path: 'edit-image', component: EditImageComponent},
+    { path: 'edit-image',
+        loadComponent: () => import('./admin/edit-image/edit-image.component').then(m => m.EditImageComponent)
+    },
     { path: 'theme/:id', component: ThemePlayComponent },
     { path: 'result/:id', component: ThemeResultComponent },
     { path: '**', redirectTo: '' }
