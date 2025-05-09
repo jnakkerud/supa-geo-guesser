3f249803058a618a9d839a02f6befcad8130a345
Jon Nakkerud <jon@nubotech.com>
2025-05-09 15:28:14 -0700

    refactor localstore

--- src/app/core/score-store-service/score-store.service.ts
+++ src/app/core/score-store-service/score-store.service.ts
@@ -7,34 +7,46 @@ export interface TotalResult extends TotalScore {
     id?: number;
 }
 
-let nextId = 0;
-const results = new Map<number, TotalResult>();
-
 const EMPTY_THEME: Theme = {
     id: 0,
     name: 'Empty Theme',
     sourceType: SourceType.FLICKR
 }
 
-@Injectable({providedIn: 'root'})
-export class ScoreStoreService {
+export class LocalScoreStoreService {
+    nextId = 0;
+    results = new Map<number, TotalResult>();
 
     public save(result: TotalScore): Promise<TotalResult> {
         // Clone
         const newResult = Object.assign({}, result) as TotalResult;
-        newResult.id = nextId++;
+        newResult.id = this.nextId++;
 
-        results.set(newResult.id, newResult);
+        this.results.set(newResult.id, newResult);
 
         return new Promise((resolve) => resolve(newResult));        
     }
 
     public get(id: number): Promise<TotalResult> {
-        const result = results.get(id);
+        const result = this.results.get(id);
         if (result) {
             return new Promise((resolve) => resolve(result));
         }
         return new Promise((reject) => reject({total:0,theme:EMPTY_THEME}));
+    }
+}
+
+@Injectable({providedIn: 'root'})
+export class ScoreStoreService {
+
+    localStore = new LocalScoreStoreService();
+
+    public save(result: TotalScore): Promise<TotalResult> {
+        return this.localStore.save(result);
+    }
+
+    public get(id: number): Promise<TotalResult> {
+        return this.localStore.get(id);
     }
     
 }
