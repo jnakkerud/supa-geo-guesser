87a50aad290bce240a79ee91bfaf532b7a8c2cf8
Jon Nakkerud <jon@nubotech.com>
2025-05-08 17:21:41 -0700

    Update player name in the db

--- src/app/core/player-service/player.service.ts
+++ src/app/core/player-service/player.service.ts
@@ -1,29 +1,60 @@
 import { Injectable } from '@angular/core';
+import { SupabaseService } from '../supabase-service/supabase.service';
+import { handleError } from '../utils';
 
+const LOCAL_PLAYER_ID = 'supa-geo-player';
 
-export const ANONYMOUS_PLAYER = {
-    id: 1,
-    name: 'Anonymous'
-};      
-
+export const ANONYMOUS_PLAYER_ID = '1';
 export interface Player {
     id: number;
     name: string;
+    canScore: boolean;
 }
 
-@Injectable({
-    providedIn: 'root',
-})
+@Injectable({providedIn: 'root',})
 export class PlayerService {
 
-    constructor() {}
+    constructor(private supabaseService: SupabaseService) {}
 
-    addPlayer(playerName: string): void {
-        // TODO
+    async addPlayer(playerName: string): Promise<Player> {
+        try{
+            const result = await this.insert(playerName);
+            const player = result[0]
+            localStorage.setItem(LOCAL_PLAYER_ID, player.id.toString());
+            return new Promise((resolve) => { resolve(player) });
+        }
+        catch(error){
+            if (error && typeof error === 'object' && 'code' in error && (error as { code: string }).code === '23505') {
+                throw new Error('Player name already exists');
+            }
+            throw error;
+        }        
     }
 
-    getPlayer(): Player {
-        // TODO
-        return ANONYMOUS_PLAYER;
+    async getPlayer(): Promise<Player> {
+        // TODO handle bad id number, rare
+        const playerId = localStorage.getItem(LOCAL_PLAYER_ID) ?? ANONYMOUS_PLAYER_ID;
+        const { data, error } = await this.supabaseService.supabase
+            .from('player')
+            .select(`
+          name,
+          id,
+          canScore:can_score
+        `).eq('id', +playerId).single();
+
+        handleError('', error);
+        return new Promise((resolve) => {resolve(data as Player)});    
+    }
+
+    private async insert(playerName: string): Promise<Player[]> {
+        const { data, error } = await this.supabaseService.supabase
+            .from('player')
+            .insert([
+                { name: playerName}
+            ]).select();
+
+        handleError('Insert Player Error', error);
+        let result: Player[] = Object.assign([] as Player[], data);
+        return new Promise((resolve) => { resolve(result) });
     }
 }
--- src/app/shared/player/player.component.ts
+++ src/app/shared/player/player.component.ts
@@ -1,6 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormControl,ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { merge } from 'rxjs';
 import { PlayerService, Player } from 'src/app/core/player-service/player.service';
 
 @Component({
@@ -8,18 +13,58 @@ import { PlayerService, Player } from 'src/app/core/player-service/player.servic
     templateUrl: 'player.component.html',
     styleUrls: ['player.component.scss'],
     standalone: true,
-    changeDetection: ChangeDetectionStrategy.OnPush,
     imports: [
         ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
         MatExpansionModule       
     ]    
 })
-export class PlayerComponent {
-    player: Player;
+export class PlayerComponent implements OnInit {
+    player: Player | null = null;
 
-    // Add your component logic here
-    constructor(playerService: PlayerService) {
-        this.player = playerService.getPlayer();
+    name = new FormControl('',[Validators.required, Validators.minLength(3),Validators.maxLength(20)]);
+
+    errorMessage = signal('');
+
+    constructor(private playerService: PlayerService) {
+        merge(this.name.statusChanges, this.name.valueChanges)
+        .pipe(takeUntilDestroyed())
+        .subscribe(() => this.updateErrorMessage());        
     }
 
+    ngOnInit(): void {
+        this.playerService.getPlayer().then((player) => {
+            this.player = player;
+        }).catch((error) => {
+            console.error('Error getting player', error.message);
+        });
+    }
+
+    savePlayerName(): void {
+        if (this.name.valid) {
+            const playerName = this.name.value;
+            this.playerService.addPlayer(playerName!).then((result) => {
+                this.player = result as Player;                
+            }).catch((error) => {
+                if (error instanceof Error) {
+                    // Invalidate the name field with error from server
+                    this.name.setErrors({ 'server': error.message });
+                }
+            });
+        }            
+    }
+
+    updateErrorMessage() {
+        if (this.name.hasError('required')) {
+            this.errorMessage.set('You must enter a value');
+        } else if (this.name.hasError('maxlength') || this.name.hasError('minlength')) {
+            this.errorMessage.set('Name must be between 3 and 20 characters');
+        } else if (this.name.hasError('server')) {
+            this.errorMessage.set(this.name.getError('server'));
+        } else {
+            this.errorMessage.set('');
+        }
+    }
 }
